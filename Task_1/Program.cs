a7d641b99ce07d2d84c707b9fa47579f11dd9e32
Kirill <kz@selfiemosaic.ru>
2023-12-27 02:24:05 +0300

    Add some var Lesson1:Task1

--- Task_1/Program.cs
+++ Task_1/Program.cs
@@ -1,1 +1,11 @@
-﻿Console.WriteLine("Hello world 25");
+﻿// Console.WriteLine("Hello world 25");
+using System.Diagnostics;
+
+int value = 100;
+Console.WriteLine(value);
+double d = 5.5555;
+char c = 'k';
+bool b = true;
+Console.WriteLine(d);
+Console.WriteLine(c);
+Console.WriteLine(b);
